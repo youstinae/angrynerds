6d62a4af1826c963560e71d5d036e2472d8b566a
mghz <m1gharzed@gmail.com>
2019-03-12 22:53:31 -0400

    updated error pages

--- hotel/routes/error.py
+++ hotel/routes/error.py
@@ -5,19 +5,25 @@ from flask import Blueprint, render_template, request
 error = Blueprint('error', __name__, url_prefix='/error')
 
 
+@error.errorhandler(403)
+def error_404(error):
+    log.error('Forbidden: %s', (request.path, error))
+    return render_template('error/error_403.html'), 403
+
+
 @error.errorhandler(404)
 def error_404(error):
     log.error('Page not found: %s', (request.path, error))
-    return render_template('error_404.html'), 404
+    return render_template('error/error_404.html'), 404
 
 
 @error.errorhandler(500)
 def error_500(error):
     log.error('Server Error: %s', (error))
-    return render_template('500.html'), 500
+    return render_template('error/error_500.html'), 500
 
 
 @error.errorhandler(Exception)
-def error_unhandled(error):
+def error(error):
     log.error('Unhandled Exception: %s', (error))
-    return render_template('error_500.html'), 500
+    return render_template('error/error.html'), 500
