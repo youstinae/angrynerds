8f60ddb0f16a306aabdc4cb2a0913b09fbacbf79
mghz <m1gharzed@gmail.com>
2019-03-18 19:55:03 -0400

    update init script

--- start.ps1
+++ start.ps1
@@ -1,4 +1,13 @@
 
+# FakeSMTP Website
+# http://nilhcem.com/FakeSMTP/index.html
+
+# to output emails in a directory
+# java -jar fakeSMTP-2.0.jar -o output_directory_name
+
+# to output email in memory
+java -jar ./tools/fakeSMTP-2.0.jar -m -s
+
 
 $env:FLASK_CONFIG=dev
 $env:FLASK_DEBUG=1
--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+
+# FakeSMTP Website
+# http://nilhcem.com/FakeSMTP/index.html
+
+# to output emails in a directory
+# java -jar fakeSMTP-2.0.jar -o output_directory_name
+
+# to output email in memory
+java -jar ./tools/fakeSMTP-2.0.jar -m -s &
+
+# Start Flask
 export FLASK_CONFIG=dev
 export FLASK_DEBUG=1
 flask run
